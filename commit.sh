7935995d4503eb183f0fab40a66c33c23a27b2f0
investment-decision <investment.decisions.biz@gmail.com>
2026-01-22 10:30:21 +0900

    Automated minification commit

--- commit.sh
+++ commit.sh
@@ -1,1 +1,7 @@
+minify ./src/index.js > ./index.min.js
+minify ./src/index.html > ./index.html
+minify ./src/index.css > ./index.css
+
+git add .
+git commit -m "Automated minification commit"
 git pull --rebase origin main && git push origin main
